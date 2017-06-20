4048c82a7998806c731cc4b6505edfe1b4cd1f2b
object-not-found <439110898@qq.com>
2017-06-20 22:15:52 +0800

    。。。

--- src/main/java/com/app/demo/conf/ShiroRealm.java
+++ src/main/java/com/app/demo/conf/ShiroRealm.java
@@ -3,6 +3,7 @@ package com.app.demo.conf;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
@@ -28,6 +29,8 @@ public class ShiroRealm extends AuthorizingRealm {
 
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		String username = (String) token.getPrincipal();
+		UsernamePasswordToken tt = (UsernamePasswordToken) token;
 		return null;
 	}
 	
